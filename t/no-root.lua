a2042fcb2be45191f7ff23f76f6fd78fcfe01f8d
nic-chen <johz@163.com>
2020-08-18 15:14:46 +0800

    add more test cases

---
+++ t/no-root.lua
@@ -0,0 +1,54 @@
+local script_generator = require 'script-generator'
+
+local script = [[
+{
+    "rule":{
+        "11-22-33-44":[
+            [
+                "code == 503",
+                "yy-uu-ii-oo"
+            ],
+            [
+                "",
+                "vv-cc-xx-zz"
+            ]
+        ]
+    },
+    "conf":{
+        "11-22-33-44":{
+            "name": "limit-count",
+            "conf": {
+                "count":2,
+                "time_window":60,
+                "rejected_code":503,
+                "key":"remote_addr"
+            }
+        },
+        "yy-uu-ii-oo":{
+            "name": "response-rewrite",
+            "conf": {
+                "body":"request has been limited",
+                "headers":{
+                    "X-limit-status": "limited"
+                }
+            }
+        },
+        "vv-cc-xx-zz":{
+            "name": "response-rewrite",
+            "conf": {
+                "body":"normal request",
+                "headers":{
+                    "X-limit-status": "normal"
+                }
+            }
+        }
+    }
+}
+]]
+
+local _, err = script_generator.generate(script)
+if err == nil then
+    error("should return error here.")
+end
+
+ngx.say("test passed.")
